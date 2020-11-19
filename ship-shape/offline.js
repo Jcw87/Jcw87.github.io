e6bb3f01d2293584086a289c642bb59a072d03b7
Jcw87 <Jcw87@users.noreply.github.com>
2020-11-18 19:53:22 -0800

    Add "Ship Shape" music thingy

---
+++ ship-shape/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1605757590,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/pressuremeter-sheet0.png",
+		"images/pressurewalrus-sheet0.png",
+		"media/shipshape.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
