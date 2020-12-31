b3629c70f8962b80fcf70dcaa40b0016aba8fb2e
Jcw87 <Jcw87@users.noreply.github.com>
2020-12-30 18:15:35 -0800

    add webgl test

---
+++ webgl/offline.js
@@ -0,0 +1,15 @@
+﻿{
+	"version": 1609381000,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
