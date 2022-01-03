922f5bf8d5dd8dab60b50adc865d4462425d1e39
Jcw87 <Jcw87@users.noreply.github.com>
2022-01-03 03:06:05 -0800

    Add analog clock

---
+++ analog-clock/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1641207520,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/clockface-sheet0.png",
+		"images/minutehand-sheet0.png",
+		"images/hourhand-sheet0.png",
+		"images/secondhand-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
